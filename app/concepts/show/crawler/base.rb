79c245f0a1018362ce6a6007b805de2743c8b0ac
Philipp Winkler <philoc@freenet.de>
2021-01-23 19:35:15 +0100

    [TASK] add config for webdriver chrome stuff on heroku

--- app/concepts/show/crawler/base.rb
+++ app/concepts/show/crawler/base.rb
@@ -2,8 +2,16 @@ module Show::Crawler
   class Base
 
     def setup_capybara(url:)
+      options = Selenium::WebDriver::Chrome::Options.new
+      if Rails.env.production?
+        Selenium::WebDriver::Chrome.path = ENV["GOOGLE_CHROME_SHIM"]
+      end
+      options.add_argument("--headless")
+      options.add_argument("--disable-dev-shm-usage")
+      options.add_argument("--no-sandbox")
+      @driver = Selenium::WebDriver.for :selenium_chrome_headless, options: options
       Capybara.run_server = false
-      Capybara.current_driver = :selenium_headless
+      Capybara.current_driver = @driver
       Capybara.app_host = url
     end
   end
